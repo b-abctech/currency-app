4a48645f082ff8395415a20685d31ae9c779fb3c
vivizth <vivizth@gmail.com>
2014-09-19 17:10:27 +0700

    add style

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,10 +5,9 @@ var currency = require('currency');
 $.btn_convert.addEventListener( 'click', function(){
 	currency.getRate({
 		success: function( data ) {
-			$.lbl_result.text = $.txt_source_money.value * data.rate;
+			$.lbl_result.text = $.txt_source_money.value * data.rate + ' ฿';
 		},
 		error: function( error_msg ) {
-			
 		}
 	}, $.txt_source_currency.value , $.txt_target_currency.value );
 });
